fe576e193cdb3b2448aaa9d669e5eaea04c24e4a
randyzwitch <rzwitch@gmail.com>
2022-05-17 08:35:04 -0400

    First two loaded tables

---
+++ dataeng/table_load/planet_osm_point.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import snowflake.connector
+import streamlit as st
+
+conn = snowflake.connector.connect(**st.secrets['sfdevrel'])
+conn.cursor().execute("USE WAREHOUSE RZ_S")
+conn.cursor().execute("USE DATABASE ZWITCH_DEV_WORKSPACE")
+conn.cursor().execute("USE SCHEMA ZWITCH_DEV_WORKSPACE.testschema")
+
+
+conn.cursor().execute("PUT file:///Users/rzwitch/planet_osm_point_us.csv.gz @%planet_osm_point")
+conn.cursor().execute("""COPY INTO planet_osm_point
+                       file_format = (TYPE = CSV, 
+                                      SKIP_HEADER=1, 
+                                      FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+                                      )
+                        ON_ERROR = CONTINUE
+                      """)
