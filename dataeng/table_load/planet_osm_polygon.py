e7f1a9bfbb9e16e491e3156d9900fed1751c2c99
randyzwitch <rzwitch@gmail.com>
2022-05-17 16:02:05 -0400

    Last two table load statements

---
+++ dataeng/table_load/planet_osm_polygon.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import snowflake.connector
+import streamlit as st
+
+from os import listdir
+
+conn = snowflake.connector.connect(**st.secrets['sfdevrel'])
+conn.cursor().execute("USE WAREHOUSE RZ_S")
+conn.cursor().execute("USE DATABASE ZWITCH_DEV_WORKSPACE")
+conn.cursor().execute("USE SCHEMA ZWITCH_DEV_WORKSPACE.testschema")
+
+flist = listdir("/Users/rzwitch/planet_osm_polygon")
+for f in flist:
+  conn.cursor().execute(f"PUT file:///Users/rzwitch/planet_osm_polygon/{f} @%planet_osm_polygon")
+  print(f + " loaded successfully")
+
+conn.cursor().execute("""COPY INTO planet_osm_polygon
+                       file_format = (TYPE = CSV, 
+                                      SKIP_HEADER=1, 
+                                      FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+                                      )
+                        ON_ERROR = CONTINUE
+                      """)
