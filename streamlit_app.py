f1a21b7dffa633d342bfa1486c94a95432ce2867
Zachary Blackwood <zachary@streamlit.io>
2022-05-26 15:18:56 -0400

    Immediately populate map when loading (#5)
    * Immediately populate app
    
    * Don't unnecessarily rerun

--- streamlit_app.py
+++ streamlit_app.py
@@ -333,11 +333,19 @@ if feature_collection:
 
 map_data = st_folium(m, width=1000, key="hard_coded_key")
 
-if "map_data" not in st.session_state:
+if (
+    "map_data" not in st.session_state
+    or st.session_state["map_data"]["bounds"]["_southWest"]["lat"] is None
+):
     st.session_state["map_data"] = map_data
 
 st.expander("Show map data").json(map_data)
 
+if st.session_state["points"].empty:
+    get_data_from_map_data(map_data, tbl, col_selected, num_rows)
+
 
 if st.button("Update data"):
     get_data_from_map_data(map_data, tbl, col_selected, num_rows)
+
+st.expander("Show session state").write(st.session_state)
